8bbc7d9835587fb4ae20c5166f994aa73b116e8d
miku12433 <kumiku2420@gmail.com>
2025-05-14 15:44:38 +0900

    Add files via upload

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,15 +1,15 @@
-let num = 45;
+let num = 15;
 
-if (num % 3 == 0 && num % 5 != 0) {
+if (num % 3 == 0 && num % 5 == 0) {
+    console.log('3と5倍数です');
+}
+
+else if (num % 3 == 0 && num % 5 != 0) {
     console.log('3の倍数です');
 }
 
 else if (num % 5 == 0 && num % 3 != 0) {
     console.log('5の倍数です');
-}
-
-else if (num % 3 == 0 && num % 5 == 0) {
-    console.log('3と5倍数です');
 }
 
 else {
